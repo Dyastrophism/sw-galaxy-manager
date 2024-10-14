987db1c4ae4719033426603847cb0ece77ef44ed
Dyastrophism <guilhermesouzamaciel1@gmail.com>
2024-10-14 10:39:59 -0300

    project  creation

---
+++ src/main/java/com/guilherme_maciel/sw_galaxyy_manager/SwGalaxyyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme_maciel.sw_galaxyy_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwGalaxyyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwGalaxyyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme_maciel/sw_galaxyy_manager/SwGalaxyyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme_maciel.sw_galaxyy_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwGalaxyyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
